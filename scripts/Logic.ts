92f4d368f40d05ba0203217a54bfad6017ea0ee9
Nathanael Axel <axel@Nathanaels-MacBook-Air.local>
2023-10-26 16:14:52 +0800

    added more backend features

--- scripts/Logic.ts
+++ scripts/Logic.ts
@@ -9,6 +9,7 @@ interface User extends Signer {
 
     isSeller: boolean; 
     sellerData: SellerData; 
+    buyerData: BuyerData;
 }
 
 interface SellerData {
@@ -16,121 +17,229 @@ interface SellerData {
     auction: DutchAuction | null;
 }
 
-
-// Initialize an array to store registered users
-const users: User[] = [];
-
-// Function to handle user registration
-function registerUser(username: string, role: "seller" | "buyer"): void {
-    if (users.find((u) => u.username === username)){
-        console.log(`Sorry, this username is taken`);
-        return;
-    }
-
-    let sellerData: SellerData | null = null;
-
-    if (role == "seller") {
-        sellerData = { token: null, auction: null };
-    } 
-
-    // Create a new user with role-specific data
-    const newUser: User = {
-        username,
-        role,
-        isSeller: role === "seller",
-        sellerData,
-    } as User;
-
-    users.push(newUser);
-    console.log(`User ${username} registered as a ${role}`);
+interface BuyerData {
+    auction: DutchAuction | null;
 }
 
-// Function to handle login
-function loginUser(username: string, role: "seller" | "buyer"): boolean {
-    const user = users.find((u) => u.username === username && u.role === role);
-    if (user) {
-        console.log(`Welcome, ${user.username}! You are logged in as a ${user.role}`);
+class Exchange{
+    users: User[] = [];
+
+    constructor(){
+        
+    }
+    
+    // Function to handle user registration
+    registerUser(username: string, role: "seller" | "buyer"): void {
+        if (this.users.find((u) => u.username === username)){
+            console.log(`Sorry, this username is taken`);
+            return;
+        }
+
+        let sellerData: SellerData | null = null;
+        let buyerData: BuyerData | null = null;
+
+        if (role == "seller") sellerData = { token: null, auction: null };
+        else if (role == "buyer") buyerData = { auction: null };
+
+        // Create a new user with role-specific data
+        const newUser: User = {
+            username,
+            role,
+            isSeller: role === "seller",
+            sellerData,
+            buyerData,
+        } as User;
+
+        this.users.push(newUser);
+        console.log(`User ${username} registered as a ${role}`);
+    }
+
+    // Function to handle login
+    loginUser(username: string, role: "seller" | "buyer"): boolean {
+        const user = this.users.find((u) => u.username === username && u.role === role);
+        if (user) {
+            console.log(`Welcome, ${user.username}! You are logged in as a ${user.role}`);
+            return true;
+        } 
+        else {
+            console.log("Login failed. Please check your username and role.");
+            return false;
+        }
+    }
+
+    async setETHBalance(user: User, amount: number): Promise<boolean>{
+        try{
+            await network.provider.send('hardhat_setBalance', [user, 1000000000000000000 * amount]); // 1 ETH in Wei
+        }
+        catch{
+            throw new Error("Failed to set ETH Balance");
+        }
         return true;
-    } 
-    else {
-        console.log("Login failed. Please check your username and role.");
-        return false;
     }
-}
 
-async function setETHBalance(user: User, amount: number): Promise<boolean>{
-    await network.provider.send('hardhat_setBalance', [user, 1000000000000000000 * amount]); // 1 ETH in Wei
-    return true;
-}
-
-// Function to check and display ETH balance
-async function getETHBalance(user: User): Promise<bigint> {
-    const balance = await ethers.provider.getBalance(user);
-    return balance;
-}
-
-// Function to check and display AxelToken balance
-async function getAxelTokenBalance(user: User): Promise<bigint> {
-    if (user && user.role === "seller" && user.sellerData.token) {
-        const token = user.sellerData.token;
-        const balance = await token.balanceOf(user);
+    // Function to check and display ETH balance
+    async getETHBalance(user: User): Promise<bigint> {
+        let balance;
+        try{
+            balance = await ethers.provider.getBalance(user);
+        }
+        catch{
+            throw new Error("Failed to get ETH Balance");
+        }
         return balance;
     }
-    else {
-        throw new Error("Cannot get AxelToken Balance.");
+
+    // Function to check and display AxelToken balance
+    async getAxelTokenBalance(user: User): Promise<bigint> {
+        if (user && user.role === "seller" && user.sellerData.token) {
+            const token = user.sellerData.token;
+            let balance;
+            try{
+                balance = await token.balanceOf(user);
+            }
+            catch{
+                throw new Error("Failed to get AxelToken Balance");
+            }
+            return balance;
+        }
+        else {
+            throw new Error("Cannot get AxelToken Balance.");
+        }
+    }
+
+    // Function to create AxelToken
+    async mintAxelToken(user: User, amount: Number): Promise<boolean>{
+        if (user && user.role === "seller") {
+            if (user.sellerData.token) throw new Error("You have created a token.");
+            let token;
+            try{
+                token = await ethers.deployContract("AxelToken", [amount], user); 
+                await token.waitForDeployment();
+                user.sellerData.token = token;
+            }
+            catch(error){
+                console.log(error);
+            }
+            return true;
+        }
+        else {
+            throw new Error("Cannot mint token.");
+        }
+    }
+
+    // Function to start DutchAuction
+    async startAuction(user: User, startingPrice: Number, reservePrice: Number, duration: Number): Promise<boolean>{
+        if (user && user.role === "seller") {
+            if (user.sellerData.token == null) throw new Error("Need to create token first.");
+            if (user.sellerData.auction) throw new Error("You have started an auction.");
+
+            let auction;
+            try{
+                auction = await ethers.deployContract(
+                    "DutchAuction", 
+                    [ startingPrice, reservePrice, user.sellerData.token, duration ], 
+                    user
+                );
+                await auction.waitForDeployment();
+            }
+            catch(error){
+                console.log(error);
+                throw new Error("Cannot deploy auction");
+            }
+
+            const token = user.sellerData.token;
+            const balance = await this.getAxelTokenBalance(user);
+
+            try{
+                await token.connect(user).approve(auction, balance);
+            }
+            catch(error){
+                console.log(error);
+                throw new Error("Cannot approve ETH to smart contract");
+            }
+
+            user.sellerData.auction = auction;
+            return true;
+        }
+        else{
+            throw new Error("Cannot start auction.");
+        }
+    }
+
+    // Function to transfer token if you win
+    async transferToken(user: User, auction: DutchAuction): Promise<boolean>{
+        if (user && user.role === "buyer") {
+            if (user.buyerData.auction == null) throw new Error("You have not joined an auction.");
+            const auction = user.buyerData.auction;
+            const zeroAddress = '0x0000000000000000000000000000000000000000';
+            try{
+                await auction.connect(user).withdrawTokens(zeroAddress);
+            }
+            catch(error){
+                console.log(error);
+            }
+            return true;
+        }
+        else{
+            throw new Error("Cannot transfer.");
+        }
+    }
+
+    // Function to transfer all tokens to winners after DutchAuction ends
+    async transferAllTokens(user: User): Promise<boolean>{
+        if (user && user.role === "seller") {
+            if (user.sellerData.auction == null) throw new Error("You have not started an auction.");
+            const auction = user.sellerData.auction;
+            try{
+                await auction.connect(user).transferAllTokens();
+            }
+            catch (error){
+                console.log(error);
+            }
+            return true;
+        }
+        else{
+            throw new Error("Cannot transfer.");
+        }
+    }
+
+    // Function to get all auctions that have started
+    getAllAuctions(): DutchAuction[]{
+        const auctions = this.users.flatMap(user => user.sellerData?.auction || []);
+        return auctions;
+    }
+
+    // Function to join auction
+    joinAuction(user: User, auction: DutchAuction): boolean{
+        const auctions = this.getAllAuctions();
+        if (!(auctions).includes(auction)) throw new Error("Auction does not exist")
+        if (user && user.role == "buyer"){
+            if (user.buyerData.auction) throw new Error("You have joined an auction.");
+            user.buyerData.auction = auction;
+            return true;
+        }
+        else{
+            throw new Error("Cannot join auction.");
+        }
+    }
+
+    async placeBid(user: User, amount: number): Promise<boolean>{
+        if (user && user.role == "buyer"){
+            if (user.buyerData.auction == null) throw new Error("You have not joined an auction.");
+            const auction = user.buyerData.auction;
+            try{
+                const ethersamount = ethers.parseEther(amount.toString());
+                await auction.connect(user).placeBid({value: ethersamount});
+            }
+            catch(error){
+                console.log(error);
+                throw new Error("Failed to place bid");
+            }
+            return true;
+        }
+        else{
+            throw new Error("Cannot place bid.");
+        }
     }
 }
-
-// Function to create AxelToken
-async function mintAxelToken(user: User, amount: Number): Promise<boolean>{
-    if (user && user.role === "seller") {
-        if (user.sellerData.token) throw new Error("You have created a token.");
-        const token = await ethers.deployContract("AxelToken", [amount], user); 
-        await token.waitForDeployment();
-        user.sellerData.token = token;
-        return true;
-    }
-    else {
-        throw new Error("Cannot mint token.");
-    }
-}
-
-// Function to start DutchAuction
-async function startAuction(user: User, startingPrice: Number, reservePrice: Number, duration: Number): Promise<boolean>{
-    if (user && user.role === "seller") {
-        if (user.sellerData.token == null) throw new Error("Need to create token first.");
-        if (user.sellerData.auction) throw new Error("You have started an auction.");
-        const auction = await ethers.deployContract(
-            "DutchAuction", 
-            [ startingPrice, reservePrice, user.sellerData.token, duration ], 
-            user
-        );
-        await auction.waitForDeployment();
-
-        const token = user.sellerData.token;
-        const balance = await getAxelTokenBalance(user);
-        await token.connect(user).approve(auction, balance);
-
-        user.sellerData.auction = auction;
-        return true;
-    }
-    else{
-        throw new Error("Cannot start auction.");
-    }
-}
-
-// Function to transfer all tokens to winners after DutchAuction ends
-async function transferAllTokens(user: User): Promise<boolean>{
-    if (user && user.role === "seller") {
-        if (user.sellerData.auction == null) throw new Error("You have not started an auction.");
-        const auction = user.sellerData.auction;
-        await auction.connect(user).transferAllTokens();
-        return true;
-    }
-    else{
-        throw new Error("Cannot transfer.");
-    }
-}
-
-
 
