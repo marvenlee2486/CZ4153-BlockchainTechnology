569a69b985f159bbdbb637859100f9fbc4b098b0
Zong Yu Lee <leezongyu6878@gmail.com>
2023-10-29 17:26:54 +0800

    Refactor Code to State Machine Pattern

--- contracts/DutchAuction.sol
+++ contracts/DutchAuction.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.6.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "hardhat/console.sol";
+import "./ErrorDutchAuction.sol";
 // TODO REFACTOR
 // 1. REFACTOR the update logic as its cost a lot of gas if everytime it is revoked (considered caching it)
 // 2. REFACTOR the require to be revert with Custom error (require will return error(string) that actually cost more)
@@ -23,6 +24,13 @@ contract DutchAuction {
         uint256 tokenleft
     );
 
+    enum Stages {
+        AuctionConstructed,
+        AuctionStarted,
+        RevealClearingPrice,
+        AuctionEnded
+    }
+
     ERC20Burnable public immutable token;
     uint256 public immutable tokenAmount;
     uint256 public tokenLeft;
@@ -34,19 +42,54 @@ contract DutchAuction {
     uint256 public immutable startingPrice;
     uint256 public immutable reservePrice; 
     uint256 public immutable discountRate;
-    uint256 public earlierClearingPrice;
+    uint256 public clearingPrice;
     uint256 public startAt;
     uint256 public expiresAt;
     uint256 public immutable duration;
     
-    bool auctionStarted = false;
-    bool auctionEndedEarly = false;
-    bool endedEarlyTemp = false;
-    bool hasBurnedUnsoldTokens = false;
+    Stages public stage = Stages.AuctionConstructed;
     uint256 revenue = 0;
     
     // TODO add token amount, I think it would be best for user to choose how many token instead of all token is bid
     // TODO do we actually need duration as parameter? I know it is better but the documentation said that 20 mins
+    modifier atStage(Stages stage_) {
+        if (stage != stage_)
+            revert FunctionInvalidAtThisStage();
+        _;
+    }
+
+    modifier auctionStart(){
+        if (stage == Stages.AuctionConstructed) 
+            revert FunctionInvalidAtThisStage();
+        _;
+    }
+
+    function _burnUnusedToken() internal{
+        _updateTokenAmount();
+        token.burn(tokenLeft);
+    }
+
+    function nextStage() internal {
+        stage = Stages(uint(stage) + 1);
+        if (stage == Stages.RevealClearingPrice){
+            if (block.timestamp >= expiresAt)
+                clearingPrice = reservePrice;
+            else 
+                clearingPrice = getPrice();
+            nextStage();
+            _burnUnusedToken();
+        }
+    }
+
+    // Perform timed transitions. Be sure to mention
+    // this modifier first, otherwise the guards
+    // will not take the new stage into account.
+    modifier timedTransitions() {
+        if (stage == Stages.AuctionStarted && block.timestamp >= expiresAt)
+            nextStage();
+        // The other stages transition by transaction
+        _;
+    }
 
     constructor(uint256 _startingPrice, uint256 _reservePrice, address _token, uint256 _duration) {
         require(_startingPrice >= _reservePrice, "Starting price must be equal / larger than ending price");
@@ -72,21 +115,23 @@ contract DutchAuction {
         _;
     }
 
-    function startAuction() public{
+    function startAuction() public atStage(Stages.AuctionConstructed){
         require(token.transferFrom(owner, address(this), tokenAmount), "Token transfer failed");
-        require(!auctionStarted, "Auction has started");
+        
     
         startAt = block.timestamp;
         expiresAt = block.timestamp + duration;
-        auctionStarted = true;
+        
+        nextStage();
     }   
 
-    function getPrice() public view returns (uint256) {
-        require(auctionStarted, "Auction has not started");
-        if (endedEarlyTemp) 
-            return earlierClearingPrice;
-        else if (auctionEndedEarly || block.timestamp >= expiresAt) 
-            return Math.max(revenue / tokenAmount, reservePrice);
+    function getPrice() public auctionStart view returns (uint256) {
+        // WORK AROUND TODO DELETE WHEN Floating point issue is solved
+        if (block.timestamp >= expiresAt)
+            return reservePrice;
+        //
+        if (stage == Stages.AuctionEnded)
+            return clearingPrice;
         else
             return startingPrice - discountRate * (block.timestamp - startAt);
     }
@@ -97,13 +142,12 @@ contract DutchAuction {
         return tokenLeft;
     }
 
-    function getPosition() external view returns (uint256) {
-        require(auctionStarted, "Auction has not started");
+    function getPosition() external auctionStart view returns (uint256) {
         return buyersPosition[msg.sender];
     }
 
     function _updateTokenAmount() internal{
-        require(auctionStarted, "Auction has not started");
+        // require(auctionStarted, "Auction has not started");
         
         uint256 currentPrice = getPrice();
         
@@ -114,20 +158,17 @@ contract DutchAuction {
         
         if (tempTokenLeft <= 0) {
             tokenLeft = 0;
-            auctionEndedEarly = true;
         }
         else {
             tokenLeft = uint(tempTokenLeft);
         }
     }
 
-    function placeBid() external payable {
-        require(auctionStarted, "Auction has not started");
-
+    function placeBid() external payable timedTransitions atStage(Stages.AuctionStarted) {
+        
         uint256 currentPrice = getPrice();
         _updateTokenAmount();
 
-        require(block.timestamp < expiresAt && !auctionEndedEarly, "Auction has ended");
         require(msg.value >= currentPrice, "Bid is too low");
 
         address buyer = msg.sender;
@@ -142,25 +183,17 @@ contract DutchAuction {
         uint256 refund = msg.value - bid;
         if (refund > 0) {
             payable(buyer).transfer(refund);
-            // console.log("REFUND AMOUNT");
-            // console.log(refund);
-            endedEarlyTemp = true;
-            earlierClearingPrice = currentPrice;
+            nextStage();
         }
         _updateTokenAmount();
     }
 
-    function _withdrawTokens (address buyer) private{
-        require(auctionStarted, "Auction has not started");
-        require(block.timestamp > expiresAt || auctionEndedEarly, "Auction is still ongoing");
+    function _withdrawTokens (address buyer) private timedTransitions atStage(Stages.AuctionEnded){
         require(buyersPosition[buyer] > 0, "You did not submit a valid bid or you have withdrawn your token");
         
-        uint256 clearingPrice = getPrice();
         uint256 bid = buyersPosition[buyer];
         uint256 tokenBought = bid / clearingPrice;
         uint256 amountPaid = tokenBought * clearingPrice;
-        //make sure that unsold tokens has been burned
-        if (!hasBurnedUnsoldTokens) _burnUnusedToken();
 
         //clear records and transfer token to buyer
         buyersPosition[buyer] = 0;
@@ -174,23 +207,18 @@ contract DutchAuction {
         payable(owner).transfer(amountPaid);
     }
 
-    function withdrawTokens (address buyer) public{
-        require(auctionStarted, "Auction has not started");
+    function withdrawTokens (address buyer) public timedTransitions atStage(Stages.AuctionEnded){
 
         if (msg.sender != owner)
             require(buyer == msg.sender, "Only owner of auction can withdraw tokens on winners behalf");
         else 
             buyer = msg.sender;
 
-        require(block.timestamp > expiresAt || auctionEndedEarly, "Auction is still ongoing");
         require(buyersPosition[buyer] > 0, "You did not submit a valid bid or you have withdrawn your token");
         
-        uint256 clearingPrice = getPrice();
         uint256 bid = buyersPosition[buyer];
         uint256 tokenBought = bid / clearingPrice;
         uint256 amountPaid = tokenBought * clearingPrice;
-        //make sure that unsold tokens has been burned
-        if (!hasBurnedUnsoldTokens) _burnUnusedToken();
 
         //clear records and transfer token to buyer
         buyersPosition[buyer] = 0;
@@ -204,22 +232,9 @@ contract DutchAuction {
         payable(owner).transfer(amountPaid);
     }
 
-    function transferAllTokens() external onlyOwner(){
-        require(auctionStarted, "Auction has not started");
-        require(block.timestamp > expiresAt || auctionEndedEarly, "Auction is still ongoing");
-        //make sure that unsold tokens has been burned
-        if (!hasBurnedUnsoldTokens) _burnUnusedToken();
-        
+    function transferAllTokens() external onlyOwner() timedTransitions atStage(Stages.AuctionEnded){
         for (uint256 i = 0; i < buyers.length; i++)
             if (buyersPosition[buyers[i]] > 0) 
                 _withdrawTokens(buyers[i]);
-    }
-
-    function _burnUnusedToken() internal{
-        require(auctionStarted, "Auction has not started");
-        require(!hasBurnedUnsoldTokens, "Unsold token has been burnt");
-        _updateTokenAmount();
-        token.burn(tokenLeft);
-        hasBurnedUnsoldTokens = true;
     }
 }
