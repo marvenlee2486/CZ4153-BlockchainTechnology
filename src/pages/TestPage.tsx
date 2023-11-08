0b719bb2100f045601ba5f78d5e40f994e221639
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2023-11-09 00:40:17 +0800

    Update TestPage.tsx

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -215,7 +215,9 @@ function TestPage() {
       return (
         <div className="flex flex-col w-full gap-2">
           {Object.keys(auctions).map((auctionAddress) => {
-            return <Auction auctionAddress={auctionAddress} />;
+            return (
+              <Auction key={auctionAddress} auctionAddress={auctionAddress} />
+            );
           })}
         </div>
       );
