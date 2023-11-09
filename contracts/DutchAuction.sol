d979082f174c6b7e210d97f92b62094e2aa7ecc7
Zong Yu Lee <leezongyu6878@gmail.com>
2023-11-09 23:48:16 +0800

    Change version of solidity

--- contracts/DutchAuction.sol
+++ contracts/DutchAuction.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 pragma abicoder v2;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
